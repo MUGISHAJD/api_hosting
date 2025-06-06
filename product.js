c147126709de1a822ba0f3d7d82852c2b97627a6
MUGISHAJD <mugishajeandedieu2@gmail.com>
2025-06-06 19:30:23 +0200

    Resolved JWT Middleware issues

--- product.js
+++ product.js
@@ -55,7 +55,7 @@ async function getDBConnection() {
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  //const token = authHeader && authHeader.split(" ")[1]; // Format: "Bearer <token>"
+  const token = authHeader && authHeader.split(" ")[1]; // <-- Uncommented and properly defined
 
   if (!token) return res.status(401).json({ error: "Token required" });
 
@@ -66,6 +66,7 @@ function authenticateToken(req, res, next) {
     next();
   });
 }
+
 
 
 // ---------------------------
